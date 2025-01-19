0170c0e5cac69c0d63e322e8905ee194da4c3f48
松浦　憲真 <2007helloworld@gmail.com>
2025-01-19 22:59:27 +0900

    カード表示の対応

--- setprint_maintenance/test_run_image_print.py
+++ setprint_maintenance/test_run_image_print.py
@@ -3,11 +3,11 @@ import os
 
 # サンプルデータ
 data = [
-    [0, 0],[[1, 0],[1,1],[[2, 4]],[1,3]]
+    [0],[[1, 0],[1,1,1],[[2, 0, 0],[2, 1, 0],[[3,0],[3,1]]],[1,3]],[0,2]
 ]
 
 # 現在の位置情報を保持するインデックスリスト
-current_indices = [-1]  # 必要な次元のみ保持
+current_indices = [-1]  # 必要な次元のみ保持q
 current_depth = 0
 
 # 現在のインデックスに基づいてデータを取得する関数
@@ -27,22 +27,71 @@ def list_deep_search(data,idx):
 
     return idx[:-1]
 
-# 現在の要素とその詳細を表示する関数
-def display_current_element():
-    os.system('cls' if os.name == 'nt' else 'clear')  # 画面をクリア
-    element = get_current_element(data, current_indices)
-    print("\n現在の要素を解析します:")
-    print(f"現在のインデックス: {current_indices}")
+def display_cards_horizontally(card_list, indices):
+    """
+    指定されたインデックスのカードを横に並べて表示する関数。
 
-    if isinstance(element, list):
-        print(f"次元 {len(current_indices)} → リストを検出しました。")
-        for i, item in enumerate(element):
-            if isinstance(item, list):
-                print(f"  [{i}] リスト: 次元を移動できます。")
-            else:
-                print(f"  [{i}] 値: {item}")
-    else:
-        print(f"値: {element}")
+    Parameters:
+        card_list (list): カードデザインが格納されたリスト。
+        indices (list): 表示するカードのインデックスを指定するリスト。
+    """
+    # 指定されたインデックスのカードを抽出し、行単位に分割
+    selected_cards = [card_list[i].splitlines() for i in indices]
+
+    # 各カードの最大行数を取得
+    max_lines = max(len(card) for card in selected_cards)
+
+    # 全てのカードの行を揃える
+    for card in selected_cards:
+        while len(card) < max_lines:
+            card.append(" " * len(card[0]))
+
+    # 横に並べて表示
+    for line_group in zip(*selected_cards):
+        print("   ".join(line_group))
+
+# 現在の要素とその詳細を表示する関数
+def display_current_element(data,indices):
+    #os.system('cls' if os.name == 'nt' else 'clear')  # 画面をクリア
+    print("\n現在の要素を解析します:")
+    print(f"現在のインデックス: {indices}")
+    #route_index = route_index[-1]q
+    #route_index[-1] -= 1
+    for line in range(len(indices)-1):
+        idx = indices[:line+1]
+        idx[-1] -= 1
+        element = get_current_element(data, idx)
+        
+        parent_element = get_current_element(data, idx[:-1])
+        parent_len = 0
+        for p_line in parent_element:
+            if type(p_line[0]) != list:
+                parent_len += 1
+        
+        print(str(idx[-1]+1)+'/'+str(parent_len))
+        
+        display_cards_horizontally(card_designs_half_width,element)
+        # print()
+
+        # print(line*' ' + str(idx))
+        # print(line*' ' + str(element))
+    
+
+    element = get_current_element(data, indices)
+
+    parent_element = get_current_element(data, indices[:-1])
+    parent_len = 0
+    for p_line in parent_element:
+        if type(p_line[0]) != list:
+            parent_len += 1
+    print(str(indices[-1]+1)+'/'+str(parent_len))
+
+    display_cards_horizontally(card_designs_half_width,element)
+        
+    # print()
+    # print((len(indices)-1) * ' ' + str(indices))
+    # print((len(indices)-1) * ' ' + str(element))
+
 
 # インデックスを更新する関数
 def update_indices(direction, indices):
@@ -64,28 +113,29 @@ def update_indices(direction, indices):
                 update_indices(direction, indices)
             
             else:
-
-                print('->_pri ; ', element)
+                
+                display_current_element(data,indices)
                 print()
+                print('->_pri ; ', element)
+                
+                # print()
         else:
             
-            print(indices)
+            # print(indices)
             before_element = get_current_element(data, indices)
-            print('<-_parent',element)
-            print('<-_index ',indices)
+            # print()
+            # print('<-_index  ',indices)
+            # print('   element',element)
 
-            # print('<-_pri ; ', before_element)
-
-            # print(0 <= indices[-1] <= len(pparent_element)-1)
+            indices = list_deep_search(before_element,indices)
             
-            if len(indices) >= 2:
-                print('True deep_2')
-                indices = list_deep_search(element,indices)
-
-            print('back_index',indices)
+            # print('back_index',indices)
             before_element = get_current_element(data, indices)
+
+            display_current_element(data,indices)
+            print()
             print('<-_pri ; ', before_element)
-            
+        
 
     else:
         # インデックスの範囲外
@@ -94,19 +144,19 @@ def update_indices(direction, indices):
 
             if indices[-1] > len(parent_element)-1:
                 # ( over )
-                print('out_range_(over > before)',indices)
+                # print('out_range_(over > before)',indices)
                 del indices[-1]
                 indices[-1] = indices[-1] + 1
 
-                print('out_range_(over ▷ after)',indices)
+                # print('out_range_(over ▷ after)',indices)
             else:
                 # ( Under )
                 del indices[-1]
                 indices[-1] = indices[-1] - 1
                 
                 if 0 <= indices[-1] <= len(parent_element)-1:
-                    indices = list_deep_search(parent_element,indices)
-                    print('back_index',indices)
+                    indices = list_deep_search(parent_element,indices[:-1])
+                    # print('back_index',indices)
                     
         else:
             if indices[-1] > len(parent_element)-1:
@@ -114,7 +164,7 @@ def update_indices(direction, indices):
             else:
                 indices = list_deep_search(data,[])
 
-            print('in_range_y0',indices)
+            # print('in_range_y0',indices)
 
         direction = 0
         update_indices(direction, indices)
@@ -135,6 +185,29 @@ def on_press(key):
             return False
     except AttributeError:
         pass
+
+card_designs_half_width = [
+    """
+    +---+
+    | 0 |
+    +---+
+    """,
+    """
+    +---+
+    | 1 |
+    +---+
+    """,
+    """
+    +---+
+    | 2 |
+    +---+
+    """,
+    """
+    +---+
+    | 3 |
+    +---+
+    """
+]
 
 # 初期状態を表示
 # display_current_element()
